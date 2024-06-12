eb69a5977351c36505d4f01e6945e868fec72e09
Craig Baker <craig.d.baker+github@gmail.com>
2024-06-12 10:24:33 -0500

    qrcode

--- app/routes/_marketing+/qrcode.tsx
+++ app/routes/_marketing+/qrcode.tsx
@@ -1,12 +1,10 @@
 import QrCodeWithLogo from 'qrcode-with-logos'
 
-import compass from './public/img/compass.png'
-
 export default function Qrcode() {
 	if (typeof document === 'undefined') {
 		// running in a server environment
 	} else {
-		let qrcode = new QrCodeWithLogo({
+		new QrCodeWithLogo({
 			// canvas: document.getElementById('canvas') as HTMLCanvasElement,
 			content: 'https://via.studyworkpray.org/',
 			width: 480,

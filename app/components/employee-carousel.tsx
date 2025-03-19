293290e0075586e96631ae77645b3cbc69ee6f30
Craig Baker <craig.d.baker+github@gmail.com>
2025-03-19 15:15:41 -0500

    Remove Mel Leece from the employee carousel component

--- app/components/employee-carousel.tsx
+++ app/components/employee-carousel.tsx
@@ -60,13 +60,6 @@ const board = [
 			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 	},
 	{
-		name: 'Mel Leece',
-		title: "Women's House Leader",
-		image: '/img/board/mel.leece.jpg',
-		quote:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-	},
-	{
 		name: 'Sam Pitre',
 		title: 'Employer',
 		image: '/img/board/sam.pitre.jpg',
